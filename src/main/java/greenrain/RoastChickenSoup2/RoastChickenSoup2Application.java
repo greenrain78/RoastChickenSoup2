64311d3407c63913f4d10500cba693add10293ca
daewon kim <kimdw010130@daum.net>
2021-05-18 15:40:50 +0900

    init commit? 어제밤에 해놓은건가?

---
+++ src/main/java/greenrain/RoastChickenSoup2/RoastChickenSoup2Application.java
@@ -0,0 +1,13 @@
+package greenrain.RoastChickenSoup2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoastChickenSoup2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoastChickenSoup2Application.class, args);
+	}
+
+}
---
+++ src/main/java/greenrain/RoastChickenSoup2/controller/HelloController.java
@@ -0,0 +1,17 @@
+package greenrain.RoastChickenSoup2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hellwo!!");
+        return "hello";
+    }
+}
